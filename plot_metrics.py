4787061f90a57e7d8b37016ed9574ada50a08baf
daniellaacr <101431159+daniellaacr@users.noreply.github.com>
2025-11-26 00:47:22 -0600

    plot_metrics.py
    Removed commented sections for clarity.

--- plot_metrics.py
+++ plot_metrics.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
 
-# ------- Datos por fold que tú obtuviste -------
+
 ap = [0.2679, 0.1458, 0.3026, 0.5531, 0.4787]
 precision = [0.0, 0.0, 0.20, 1.0, 1.0]
 recall = [0.0, 0.0, 0.5, 0.1667, 0.1667]
 
 folds = [1, 2, 3, 4, 5]
 
-# ---------- 1. Gráfica AP ----------
 plt.figure(figsize=(6,4))
 plt.plot(folds, ap, marker="o", linewidth=2)
 plt.title("Average Precision por Fold")
@@ -17,7 +16,7 @@ plt.grid(True)
 plt.savefig("ap_fold.png", dpi=300)
 plt.close()
 
-# ---------- 2. Gráfica Precision ----------
+
 plt.figure(figsize=(6,4))
 plt.plot(folds, precision, marker="o", color="green", linewidth=2)
 plt.title("Precision por Fold")
@@ -27,7 +26,6 @@ plt.grid(True)
 plt.savefig("precision_fold.png", dpi=300)
 plt.close()
 
-# ---------- 3. Gráfica Recall ----------
 plt.figure(figsize=(6,4))
 plt.plot(folds, recall, marker="o", color="red", linewidth=2)
 plt.title("Recall por Fold")
